14f5115cc169feca4afee3b5ea68516eac9b5475
Anondo Ray <anondo554@gmail.com>
2025-12-28 18:58:17 +0600

    server error handaler added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
 
-import { Server } from 'http';
-import mongoose from 'mongoose';
-import app from './app';
 let server: Server;
 
 const startServer = async () => {
@@ -10,7 +10,7 @@ const startServer = async () => {
       "mongodb+srv://tourmangemet:ymIww6NE1vLYi7iF@cluster0.gbp43.mongodb.net/TourManagement?appName=Cluster0"
     );
     console.log("Connected to MongoDB");
-    app.listen(3000, () => {
+    server = app.listen(3000, () => {
       console.log("Server is running on port 3000");
     });
   } catch (error) {
@@ -19,3 +19,50 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM detected....Shutting down the server...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+process.on("SIGINT", () => {
+  console.log("SIGINT detected....Shutting down the server...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+process.on("unhandledRejection", (err) => {
+  console.log(
+    "Unhandled Rejection detected....Shutting down the server...",
+    err
+  );
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(
+    " Uncaught Exception detected....Shutting down the server...",
+    err
+  );
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// Promise.reject(new Error("I forgot to catch this promise"));
+// throw new Error("Something went wrong");
