1b0dfbc2481375c0654d58312e436229721ffb37
Anondo Ray <anondo554@gmail.com>
2025-12-30 19:54:02 +0600

    bug fixt

--- src/app/errorHelpers/AppError.ts
+++ src/app/errorHelpers/AppError.ts
@@ -1,6 +1,7 @@
 class AppError extends Error {
   public statusCode: number;
 
+<<<<<<< HEAD
   constructor(statusCode: number, message: string, stark = "") {
     super(message); //thow new error ("sending error message");
     this.statusCode = statusCode;
@@ -14,3 +15,19 @@ class AppError extends Error {
 }
 
 export default AppError;
+=======
+  constructor(statusCode: number, message: string, stack="") {
+    super(message);
+      this.statusCode = statusCode;
+
+      if (stack) {
+          this.stack= stack
+      }
+      else {
+          Error.captureStackTrace(this, this.constructor);
+      }
+  }
+}
+
+export default AppError
+>>>>>>> 1c39634 (globale error handaling added)
--- src/app/middelewars/globaleErrorHandler.ts
+++ src/app/middelewars/globaleErrorHandler.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
@@ -28,3 +29,21 @@ export const globalErrorHandler = (
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
 };
+=======
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import { envVars } from "./../config/env";
+
+export const globalleErrorHandler =
+  () => (err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = 500;
+    const message = `Something went wrong ${err.message} `;
+    res.status(statusCode).json({
+      success: false,
+      message,
+      err,
+      stack: err.envVars === "production" ? err.stack : null,
+    });
+  };
+>>>>>>> 1c39634 (globale error handaling added)
